cb8522f85ac98cdc0f95d7ea652d9bfcb08d9234
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-28 22:28:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ class Config(object):
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
+    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/f2cf2b162e72124b0b329.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001596651023"))
