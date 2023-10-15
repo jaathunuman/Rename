454d2d94e2c71fe332e1af1f7c725f0ab0bc4cf3
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-15 16:23:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,5 @@
 """
-Apache License 2.0
-Copyright (c) 2022 @PYRO_BOTZ
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-Telegram Link : https://t.me/PYRO_BOTZ 
-Repo Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT
-License Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT/blob/main/LICENSE
-"""
+
 
 import re, os, time
 
@@ -28,7 +9,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "16743442")
     API_HASH  = os.environ.get("API_HASH", "12bbd720f4097ba7713c5e40a11dfd2a")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6488845260:AAHQDqLMd-iAppHbaYcaf6LkiySOi_mrvas") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6308859474:AAGTgbnyE0oMQyFuvtdSlibaZQs_7dZ_cLo") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
