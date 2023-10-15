a71cfba7413d0bdffee5c43cf942b3551a278a4b
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2023-10-15 16:37:16 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -16,27 +16,39 @@ import os, time
 
 @Client.on_message(filters.private & (filters.document | filters.audio | filters.video))
 async def rename_start(client, message):
-    file = getattr(message, message.media.value)
-    filename = file.file_name  
-    if file.file_size > 2000 * 1024 * 1024:
-         await message.reply_text("Sᴏʀʀy Bʀᴏ Tʜɪꜱ Bᴏᴛ Iꜱ Dᴏᴇꜱɴ'ᴛ Sᴜᴩᴩᴏʀᴛ Uᴩʟᴏᴀᴅɪɴɢ Fɪʟᴇꜱ Bɪɢɢᴇʀ Tʜᴀɴ 2Gʙ")
+    user = message.from_user
+    user_id = user.id
 
-    try:
-        await message.reply_text(
-            text=f"**__Pʟᴇᴀꜱᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇɴᴀᴍᴇ...__**\n\n**Oʟᴅ Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`",
-	    reply_to_message_id=message.id,  
-	    reply_markup=ForceReply(True)
-        )       
-        await sleep(30)
-    except FloodWait as e:
-        await sleep(e.value)
-        await message.reply_text(
-            text=f"**__Pʟᴇᴀꜱᴇ Eɴᴛᴇʀ Nᴇᴡ Fɪʟᴇɴᴀᴍᴇ...__**\n\n**Oʟᴅ Fɪʟᴇ Nᴀᴍᴇ** :- `{filename}`",
-	    reply_to_message_id=message.id,  
-	    reply_markup=ForceReply(True)
-        )
-    except:
-        pass
+    # Check if the user is banned
+    is_banned = await db.is_user_banned(user_id)
+
+    if is_banned:
+        # Send a message indicating that the user is banned
+        await message.reply_text("You are banned by the admin.")
+    else:
+        file = getattr(message, message.media.value)
+        filename = file.file_name
+
+        if file.file_size > 2000 * 1024 * 1024:
+            await message.reply_text("Sorry, this bot doesn't support uploading files bigger than 2GB.")
+        else:
+            try:
+                await message.reply_text(
+                    text=f"**__Please enter a new file name...__**\n\n**Old File Name** :- `{filename}`",
+                    reply_to_message_id=message.id,
+                    reply_markup=ForceReply(True)
+                )
+                await sleep(30)
+            except FloodWait as e:
+                await sleep(e.value)
+                await message.reply_text(
+                    text=f"**__Please enter a new file name...__**\n\n**Old File Name** :- `{filename}`",
+                    reply_to_message_id=message.id,
+                    reply_markup=ForceReply(True)
+                )
+            except:
+                pass
+
 
 
 
